8b271004b8e45d341da061f73e4ba87c49bcb692
YutaKawabata2002 <yutakawabata@mail.weber.edu>
2025-03-30 17:22:52 -0600

    fxed the update and create new for user page

--- RVPark/Pages/Admin/Users/Index.cshtml.cs
+++ RVPark/Pages/Admin/Users/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FoodDelivery.Pages.Admin.Users
+namespace RVPark.Pages.Admin.Users
 {
     public class IndexModel : PageModel
     {
