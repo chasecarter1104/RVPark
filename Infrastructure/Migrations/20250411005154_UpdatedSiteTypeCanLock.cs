41d2dfd3fda0760fa33e4fa4bafefb803d5b1ee1
Calvin Oats <88681685+CalvinJOats@users.noreply.github.com>
2025-04-10 23:31:24 -0600

    Changed Site Type to be lockable, removed delete button.

---
+++ Infrastructure/Migrations/20250411005154_UpdatedSiteTypeCanLock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedSiteTypeCanLock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "SiteType",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "SiteType");
+        }
+    }
+}
