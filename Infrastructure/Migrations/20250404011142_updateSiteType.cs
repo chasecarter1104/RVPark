31207a84883c71095d949c88e916e36b4b9e4d69
Peter Rosqvist <peter.rosqvist8@gmail.com>
2025-04-14 16:50:54 -0600

    rv park updated for sake of pulling

---
+++ Infrastructure/Migrations/20250404011142_updateSiteType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateSiteType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "SiteType",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: null);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "SiteType",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "SiteType");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "SiteType");
+        }
+    }
+}
