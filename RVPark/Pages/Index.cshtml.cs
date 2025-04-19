060a240702571820f78c13a1963007475f6da597
YutaKawabata2002 <yutakawabata@mail.weber.edu>
2025-04-18 23:02:33 -0600

    filter logic

--- RVPark/Pages/Index.cshtml.cs
+++ RVPark/Pages/Index.cshtml.cs
@@ -7,10 +7,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApplicationCore.Interfaces;
 using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 public class IndexModel : PageModel
 {
     private readonly UnitOfWork _unitOfWork;
+
+    [BindProperty(SupportsGet = true)]
+    public DateTime? FilterStartDate { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public DateTime? FilterEndDate { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public int? FilterSiteTypeId { get; set; }
+
+    public IEnumerable<SelectListItem> SiteTypeList { get; set; }
+
 
     public IndexModel(UnitOfWork unitOfWork)
     {
@@ -21,14 +34,49 @@ public class IndexModel : PageModel
 
     public async Task OnGetAsync()
     {
-        var today = DateTime.Today;
-
         var allSites = await _unitOfWork.Site.ListAsync(predicate: _ => true, includes: "SiteType");
-        var reservations = await _unitOfWork.Reservation
-            .ListAsync(r => r.StartDate <= today && r.EndDate >= today);
 
-        var reservedIds = reservations.Select(r => r.SiteId).ToHashSet();
+        var siteTypes = await _unitOfWork.SiteType.ListAsync();
+        SiteTypeList = siteTypes.Select(st => new SelectListItem
+        {
+            Value = st.Id.ToString(),
+            Text = st.Name
+        });
 
-        AvailableSites = allSites.Where(site => !reservedIds.Contains(site.Id)).ToList();
+        AvailableSites = (List<Site>)allSites;
+
+        if (FilterSiteTypeId.HasValue)
+        {
+            AvailableSites = AvailableSites
+                .Where(s => s.SiteTypeId == FilterSiteTypeId.Value)
+                .ToList();
+        }
+
+        if (FilterStartDate.HasValue && FilterEndDate.HasValue && FilterStartDate < FilterEndDate)
+        {
+            var reservations = await _unitOfWork.Reservation.ListAsync(r =>
+                r.StartDate < FilterEndDate && r.EndDate > FilterStartDate);
+
+            var reservedIds = reservations.Select(r => r.SiteId).ToHashSet();
+
+            AvailableSites = AvailableSites
+                .Where(site => !reservedIds.Contains(site.Id))
+                .ToList();
+        }
+        else
+        {
+            // Default to today's availability if no filter provided
+            var today = DateTime.Today;
+            var reservations = await _unitOfWork.Reservation
+                .ListAsync(r => r.StartDate <= today && r.EndDate >= today);
+
+            var reservedIds = reservations.Select(r => r.SiteId).ToHashSet();
+
+            AvailableSites = AvailableSites
+                .Where(site => !reservedIds.Contains(site.Id))
+                .ToList();
+        }
+
     }
+
 }
