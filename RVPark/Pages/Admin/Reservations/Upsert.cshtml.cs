e4cae54c9e1cb9e67534c067a7d45517e34b7170
chasecarter1104 <156862597+chasecarter1104@users.noreply.github.com>
2025-04-18 21:34:25 -0600

    reservation

--- RVPark/Pages/Admin/Reservations/Upsert.cshtml.cs
+++ RVPark/Pages/Admin/Reservations/Upsert.cshtml.cs
@@ -1,6 +1,7 @@
 using ApplicationCore.Models;
 using ApplicationCore.Interfaces;
 using Infrastructure.Data;
+using ApplicationCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -19,6 +20,7 @@ namespace RVPark.Pages.Admin.Reservations
         // Creating these for a dropdown list
         public IEnumerable<SelectListItem> SiteList { get; set; }
         public IEnumerable<SelectListItem> UserList { get; set; }
+
         // Constructor injection
         public UpsertModel(UnitOfWork unitOfWork)
         {
@@ -27,15 +29,16 @@ namespace RVPark.Pages.Admin.Reservations
 
         public void OnGet(int? id)
         {
-            var sites = _unitOfWork.Site.List(); 
-            var users = _unitOfWork.User.List(); 
+            var sites = _unitOfWork.Site.List();
+            var users = _unitOfWork.User.List();
 
             if (id != null)
             {
+                // Get the reservation if id is provided
                 Reservation = _unitOfWork.Reservation.Get(u => u.Id == id, true);
             }
 
-            if (Reservation == null) // New upsert
+            if (Reservation == null) // New reservation
             {
                 Reservation = new Reservation();
             }
@@ -46,9 +49,9 @@ namespace RVPark.Pages.Admin.Reservations
 
         public IActionResult OnPost(int? id)
         {
-            if (id == null || Reservation.Id == 0)
+            if (Reservation.Id == 0)
             {
-                // This should be a new reservation, but we want to avoid treating it as update
+                // Add new reservation to the database
                 _unitOfWork.Reservation.Add(Reservation);
             }
             else
@@ -57,14 +60,20 @@ namespace RVPark.Pages.Admin.Reservations
                 var objFromDb = _unitOfWork.Reservation.Get(r => r.Id == Reservation.Id, true);
                 if (objFromDb != null)
                 {
+                    // Update the reservation with new values
                     _unitOfWork.Reservation.Update(Reservation);
+                }
+                else
+                {
+                    // Handle case where reservation is not found (e.g., return error)
+                    return NotFound();
                 }
             }
 
             // Save changes to the database
             _unitOfWork.Commit();
 
-            // Redirect to the list page
+            // Redirect
             return RedirectToPage("./Index");
         }
     }
